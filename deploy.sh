2d90d755122119a7fc5cdd1f64293b83649678b3
molnesium <hm@nabo.as>
2019-04-16 15:40:51 +0200

    added deploy script

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+npm run-script build
+
+rm -r ../hmol.github.io/*
+cp -a ./build/. ../hmol.github.io/
+
+cd ../hmol.github.io/
+git add .
+git commit -m "new build of static content"
+git push
--- src/Components/Contactinfo/Contactinfo.js
+++ src/Components/Contactinfo/Contactinfo.js
@@ -17,21 +17,20 @@ class Contactinfo extends React.Component {
             I'm {this.getAge()} years old and live in Oslo with my two sons and girlfriend. 
             After finishing a bachelor's degree in informatics the year 2012, I started working as a consultant for Making Waves.
             I worked on three projects with them, before I made the switch in 2016 to join ITverket where I am now. 
-            </p>
-
-                <ContactItem text="Mail" url="mailto:molnes@gmail.com"></ContactItem>
-                <ContactItem text="LinkedIn" url="https://www.linkedin.com/in/henrikmolnes/"></ContactItem>
-                <ContactItem text="Github" url="https://github.com/hmol"></ContactItem>
-                <div className="resume-container">
-                    <a href="https://github.com/hmol/Curriculum-Vitae/raw/master/molnes_cv.pdf" className="resume-link">
-                        <span className="pdf-icon"></span>
-                        Download resume
-                    </a>
-                </div>
+            </p>           
+            <ContactItem text="Mail" url="mailto:molnes@gmail.com"></ContactItem>
+            <ContactItem text="LinkedIn" url="https://www.linkedin.com/in/henrikmolnes/"></ContactItem>
+            <ContactItem text="Github" url="https://github.com/hmol"></ContactItem>
+            <div className="resume-container">
+                <a href="https://github.com/hmol/Curriculum-Vitae/raw/master/molnes_cv.pdf" className="resume-link">
+                    <span className="pdf-icon"></span>
+                    Download resume
+                </a>
+            </div>
             
+            <iframe src="https://open.spotify.com/embed/user/hmol_/playlist/4o8JeOveTdtRBXFGFmeKxY" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
         </div>
-        );
-    }
+        );  }
 }
 
 export default Contactinfo;
