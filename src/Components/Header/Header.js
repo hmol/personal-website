7410e84514bec563aed3a09824b8df2f4397a06b
molnesium <hm@nabo.as>
2019-04-26 09:16:21 +0200

    removed unused

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Header.scss';
 import Face from '../Face/Face';
-class Header extends React.Component {
+    class Header extends React.Component {
 
     temperatures = [];
 
