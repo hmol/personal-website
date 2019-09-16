cf4824fcec7461e669aefb8ffcb4a51fbf68c59b
molnesium <hm@nabo.as>
2019-09-16 15:39:15 +0200

    updated link section

--- src/Components/Links/Links.tsx
+++ src/Components/Links/Links.tsx
@@ -8,15 +8,11 @@ class Links extends React.Component  {
         return (
 			<div className="contact">
 				<h2>Links</h2>
-				<div className="links">
-					<div>					
+				<div className="links">				
 					<a href="mailto:molnes@gmail.com">Mail</a>
 					<a href="https://www.linkedin.com/in/henrikmolnes">LinkedIn</a>
-					</div>
-					<div>
 					<a href="https://github.com/hmol">Github</a>
 					<a href="https://github.com/hmol/writings">Writings</a>
-					</div>
 				</div>
 		  	</div>
         );
